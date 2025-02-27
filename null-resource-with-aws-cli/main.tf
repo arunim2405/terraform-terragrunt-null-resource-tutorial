1e7ac2a39c077cff3dd7b5de42fadac58b219805
Arunim Chaudhary <arunim.chaudhary@stackguardian.io>
2025-02-27 17:57:11 +0530

    Update main.tf

--- null-resource-with-aws-cli/main.tf
+++ null-resource-with-aws-cli/main.tf
@@ -6,6 +6,6 @@ resource "null_resource" "run_aws_version" {
   depends_on = [null_resource.check_aws_version]
 
   provisioner "local-exec" {
-    command = "aws --version"
+    command = "aws rds modify-db-instance --db-instance-identifier mydbinstance --engine-version 5.6.40 --apply-immediately"
   }
 }
